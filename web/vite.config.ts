188eac134fc55190c27e30ad8ca51c45738d49d6
fateavalon <shaohenz.usc@gmail.com>
2026-03-18 23:42:10 -0700

    update config

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/douban-bibliocommons-rating/",
+  base: "/bibliocommons-search-in-douban/",
 });
